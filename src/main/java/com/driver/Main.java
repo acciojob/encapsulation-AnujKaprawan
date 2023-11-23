f0d3d3eaa3b3634045fad4033d2c388af29ac571
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2023-11-23 17:31:22 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Attempting to access 'name' directly - Error: Cannot access private member
+        // obj.name = "Direct Access"; // This will cause an error
+
+        // Attempting to print 'name' directly - Error: Cannot access private member
+        // System.out.println(obj.name); // This will cause an error
+
+        // Using setter function to set 'name'
+        obj.setName("John Doe");
+
+        // Using getter function to access 'name'
+        System.out.println("Name: " + obj.getName()); // Prints: Name: John Doe
+    }
 }
